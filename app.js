b802f3607afa6d91e57c5b9f7e0cffa68f825508
HoangTe <hanquoclythu@gmail.com>
2022-09-05 21:33:52 +0700

    ExpressJs framework

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require('http');
-const routes = require('./routes');
-const server = http.createServer(routes.handler);
-console.log(routes.someText)
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-server.listen(3000);
+const app = express();
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+app.listen(3000);
