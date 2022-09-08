c4bbb78743edaa3abe44b926535e481ea60acf40
HoangTe <hanquoclythu@gmail.com>
2022-09-09 00:01:58 +0700

    connect with Mysql

---
+++ Util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'tehv1789$$'
+});
+
+module.exports = pool.promise();
